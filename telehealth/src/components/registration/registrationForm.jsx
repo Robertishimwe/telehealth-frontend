fc2a34694a732ad6726de63e6e718bd426f287c2
robert <robertishimwe0@gmail.com>
2022-09-24 23:26:07 +0200

    f

--- telehealth/src/components/registration/registrationForm.jsx
+++ telehealth/src/components/registration/registrationForm.jsx
@@ -3,11 +3,9 @@ import { Link } from 'react-router-dom';
 
 function registrationForm() {
   return (
-    <div  style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-     
-      <Link to="/registration/patient" className='btn btn-dark rounded-pill py-3 px-5 me-3 outline-dark '>Patient</Link>
-      <Link to="/registration/hospital" className='btn btn-outline-dark rounded-pill py-3 px-5 me-3'>Hospital</Link>
-
+    <div className="mainFormContainer">
+      <div><Link to="/registration/patient" className='btn btn-dark rounded-pill py-3 px-5 me-3 outline-dark '>Patient</Link></div>
+      <div><Link to="/registration/hospital" className='btn btn-outline-dark rounded-pill py-3 px-5 me-3'>Hospital</Link></div>
     </div>
   )
 }
